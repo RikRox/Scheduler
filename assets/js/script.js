41e32daba66292ec267ac39dd0f809f17ebee99c
Rikayla <Rikaylaj@gmail.com>
2022-07-14 10:08:26 -0400

    local storage working!

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,15 @@ var today = new Date();
 
 todayEl.textContent = today;
 
+ var plans =["schedule"];
+
 
 
 //create time table
 var rowHour=8;
 
 for(var i =0; i<= 8; i++){
-    $('.container').append('<div class="row" id="'+rowHour+'"><div class="col hour"">'+rowHour+':00</div><div class="col-6 description textarea"><textarea>Text Area</textarea></div><div class="col"><button class="saveBtn" type="button">Save</button></div></div>');
+    $('.container').append('<div class="row" id="'+rowHour+'"><div class="col hour"">'+rowHour+':00</div><textarea class="col-6 description id="txt'+rowHour+'"></textarea><button class="saveBtn col" type="button">Save</button></div>');
   
     rowHour = 9 + i;
     console.log("row hour" + rowHour);
@@ -30,25 +32,110 @@ for(var i =0; i<= 8; i++){
 
 //add/edit descriptions
 // row was clicked
-$(".description").on("click", "textarea", function() {
+$(".saveBtn").on("click", function() {
    // console.log(this);
   // var text = $(this).text();
-   var text = $(this).text().trim();
+   var text = $(this).siblings('.description').val();
    console.log(text);
 
-   var textInput = $("<textarea>")
-//.attr("type", "text")
-.addClass("form-control")
-.val(text);
-$(this).replaceWith(textInput);
-textInput.trigger("focus");
+var timeNow = $(this).parent().attr("id");
+console.log(timeNow);
+
+localStorage.setItem(timeNow, text);
+
+
+
   });
 
 
 
 
-//create object to be able to use local storage
-var plans = {};
+function loadPlans(){
+   $('#8 .description').val(localStorage.getItem('8'));
+   $('#9 .description').val(localStorage.getItem('9'));
+   $('#10 .description').val(localStorage.getItem('10'));
+   $('#11 .description').val(localStorage.getItem('11'));
+   $('#12 .description').val(localStorage.getItem('12'));
+   $('#13 .description').val(localStorage.getItem('13'));
+   $('#14 .description').val(localStorage.getItem('14'));
+   $('#15 .description').val(localStorage.getItem('15'));
+   $('#16 .description').val(localStorage.getItem('16'));
+
+}
+
+loadPlans();
+
+
+//function to save events/plans/////////////////////////////////////////////////////
+
+function savePlans(){
+
+
+   plans[0] = $("#txt8").text();
+   plans[1] = $("#txt9").text();
+   plans[2] = $("#txt10").text();
+   plans[3] = $("#txt11").text();
+   plans[4] = $("#txt12").text();
+   plans[5] = $("#txt13").text();
+   plans[6] = $("#txt14").text();
+   plans[7] = $("#txt15").text();
+   plans[8] = $("#txt16").text();
+
+
+   console.log("save the plans"+plans);
+
+   console.log(plans.length);
+
+   localStorage.setItem("events",JSON.stringify(plans));
+
+
+
+}
+
+
+if (plans = ''){
+   plans.pop;
+} else {
+   plans.push;
+}
+
+
+
+
+   plans[0] = $("#txt8").text();
+   plans[1] = $("#txt9").text();
+   plans[2] = $("#txt10").text();
+   plans[3] = $("#txt11").text();
+   plans[4] = $("#txt12").text();
+   plans[5] = $("#txt13").text();
+   plans[6] = $("#txt14").text();
+   plans[7] = $("#txt15").text();
+   plans[8] = $("#txt16").text();
+
+console.log("thiisssss" + plans[0]);
+console.log("thiisssss" + plans[1]);
+console.log("thiisssss" + plans[2]);
+console.log("thiisssss" + plans[3]);
+console.log("thiisssss" + plans[4]);
+console.log("thiisssss" + plans[5]);
+console.log("thiisssss" + plans[6]);
+console.log("thiisssss" + plans[7]);
+console.log("thiisssss" + plans[8]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -59,12 +146,14 @@ var plans = {};
     $(".description").on("blur", "textarea", function() {
 
         // get the textarea's current value/text
-    var text = $(this)
+    text = $(this)
     .val()
     .trim();
 
     console.log(text);
-
+    //plans.push(text);
+    console.log(plans);
+    localStorage.setItem("event", JSON.stringify(plans));
     // get the parent ul's id attribute
     // var status = $(this)
     // .closest(".description")
@@ -76,11 +165,28 @@ var plans = {};
     // .closest(".list-group-item")
     // .index();
 
+      savePlans();
 
+
+      var plansObj = {
+         planEvent : text,
+         id : rowHour   
+      };
+
+
+      if (plansObj.planEvent = ''){
+         plansObj.planEvent.pop;
+      } else {
+         plansObj.planEvent.push;
+      }
+
+      console.log("show plans obj: " + plansObj);
 
 });
+function colorRows(){
 
 
+currentHour = today.getHours();
 //colors for 8:00
  if($("#8").text().charAt(0) == currentHour) {
 
@@ -198,6 +304,9 @@ console.log($("#10").text().substring(0,2));
 console.log(currentHour);
 
 //console.log($("div.col.hour"));
+}
 
+colorRows();
+var timeInterval = setInterval(colorRows,30000);
 
-
+savePlans();
